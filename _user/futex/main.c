90c3f4dca3ebb1bf065a89462676004844d62ab4
kamilaboba123 <kamil.kowalczyk@phoenix-rtos.com>
2025-07-09 13:31:23 +0200

    Update submodules

--- _user/futex/main.c
+++ _user/futex/main.c
@@ -9,20 +9,20 @@ volatile int futex = 0;
 void *thread1(void *arg)
 {
 	printf("thread1\n");
-	printf("Before futex_wait()\n");
+	printf("thread1: Before futex_wait()\n");
 	int err = futex_wait((unsigned int *)&futex, 0, 5);
-	printf("After futex_wait()\n");
-	printf("err = %d\n", err);
+	printf("thread1: After futex_wait()\n");
+	printf("thread1: err = %d\n", err);
 	return NULL;
 }
 
 void *thread2(void *arg)
 {
 	printf("thread1\n");
-	printf("Before futex_wakeup()\n");
+	printf("thread2: Before futex_wakeup()\n");
 	int err = futex_wakeup((unsigned int *)&futex, 1);
-	printf("After futex_wakeup()\n");
-	printf("err = %d\n", err);
+	printf("thread2: After futex_wakeup()\n");
+	printf("thread2: err = %d\n", err);
 	return NULL;
 }
 
